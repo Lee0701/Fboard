ea714e151c3f6f2221861c8dd1edfbef597bcb0d
Lee0701 <hgl1002@naver.com>
2023-06-22 02:48:58 +0900

    Make it type something

--- core/src/main/java/ee/oyatl/ime/f/core/FboardAppBase.kt
+++ core/src/main/java/ee/oyatl/ime/f/core/FboardAppBase.kt
@@ -4,8 +4,10 @@ import android.app.Application
 import com.google.android.material.color.DynamicColors
 
 abstract class FboardAppBase: Application() {
+
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
+
 }
--- ime-ko/src/main/java/ee/oyatl/ime/f/ko/FboardIMEKorean.kt
+++ ime-ko/src/main/java/ee/oyatl/ime/f/ko/FboardIMEKorean.kt
@@ -3,4 +3,5 @@ package ee.oyatl.ime.f.ko
 import ee.oyatl.ime.f.core.FboardIMEBase
 
 class FboardIMEKorean: FboardIMEBase() {
+
 }
