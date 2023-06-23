950c8d7e14625bbc98108ed6d33d4e10b54e91d6
Lee0701 <hgl1002@naver.com>
2023-06-23 11:24:07 +0900

    Add empty settings activity

--- common/src/main/java/ee/oyatl/ime/f/common/settings/SettingsActivity.kt
+++ common/src/main/java/ee/oyatl/ime/f/common/settings/SettingsActivity.kt
@@ -2,6 +2,7 @@ package ee.oyatl.ime.f.common.settings
 
 import android.os.Build
 import android.os.Bundle
+import androidx.annotation.XmlRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.preference.PreferenceFragmentCompat
@@ -9,14 +10,16 @@ import androidx.preference.PreferenceManager
 import com.google.android.material.elevation.SurfaceColors
 import ee.oyatl.ime.f.common.R
 
-class SettingsActivity: AppCompatActivity() {
+abstract class SettingsActivity: AppCompatActivity() {
+
+    abstract val prefResId: Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.settings, SettingsFragment())
+            .replace(R.id.settings, SettingsFragment(prefResId))
             .commit()
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
@@ -28,12 +31,14 @@ class SettingsActivity: AppCompatActivity() {
             insetsController.isAppearanceLightStatusBars = true
             insetsController.isAppearanceLightNavigationBars = true
         }
-        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false)
+        PreferenceManager.setDefaultValues(this, prefResId, false)
     }
 
-    class SettingsFragment: PreferenceFragmentCompat() {
+    class SettingsFragment(
+        @XmlRes val prefResId: Int,
+    ): PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            setPreferencesFromResource(prefResId, rootKey)
         }
     }
 }
---
+++ ime-ko/src/main/java/ee/oyatl/ime/f/ko/FboardSettingsKorean.kt
@@ -0,0 +1,7 @@
+package ee.oyatl.ime.f.ko
+
+import ee.oyatl.ime.f.common.settings.SettingsActivity
+
+class FboardSettingsKorean(): SettingsActivity() {
+    override val prefResId: Int = ee.oyatl.ime.f.common.R.xml.root_preferences
+}
